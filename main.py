9b654e7afcc58e19d340c4fcb655e094c5b93a95
Yavuz Selim Varol <yavuzsvarol@gmail.com>
2025-08-19 15:11:46 +0300

    parse fix

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ class Tree:
         self.data = None
 
 def parseNumber(string):
-    if string[0] == "-":
-        negative = True
+    negative = False
+    if string[0] == "-": negative = True
     i = 0
-    for a in string:
+    for char in string:
         i += 1
-        if a not in "0123456789":
+        if char not in "0123456789":
+            if string[:i] is "": return print("Beklenemeyen girdi.")
             if negative == True: return [-1*int(string[:i]), string[i:]]
             return  [int(string[:i]), string[i:]]
     
