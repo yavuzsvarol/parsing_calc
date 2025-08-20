880e9f3699373ff86581221fc0845025135609c7
Yavuz Selim Varol <yavuzsvarol@gmail.com>
2025-08-20 19:31:48 +0300

    add operation parse

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-def parseNumber(number, string):
+def parseNumber(answer, string):
     negative = False
     i = 0
     if string[0] == "-":
         negative = True
     for char in string:
         i += 1
-        if char not in "-0123456789":
-            if negative == True: return [-1*int(string[1:i]), string[i:].strip()]
+        if negative == True and i == 1:
+            continue
+        if char not in "0123456789":
             return  [int(string[:i]), string[i:].strip()]
-    
+
+def parseOperation(answer, string):
+    addition, subtract = False, False
+    i = 0
+    if string[0] == "+":
+        addition = True
+    elif string[0] == "-":
+        subtract = True
+    for char in string:
+        i += 1
+        if negative == True and i == 1:
+            continue
+        if char not in "0123456789":
+            return  [int(string[:i]), string[i:].strip()]
+
+
+# Expr = Number (("+" | "-") Number)*
+# Term = Number
+# Number = [0-9]+
 
 print("Hesap Makinesi")
 expr = input("\nLütfen işlemi giriniz:")
