372119a594de5e9f57f0328d5bce43cc508a142f
Yavuz Selim Varol <yavuzsvarol@gmail.com>
2025-08-21 13:59:01 +0300

    handle exceptions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def parseNumber(string):
+    if not string:
+        return "Hatalı girdi: Boş işlem"
     negative = False
     i = 0
     if string[0] == "-":
@@ -8,19 +10,24 @@ def parseNumber(string):
         if negative == True and i == 1:
             continue
         if char not in "0123456789":
+            i -= 1
             break
-    if string[i:].strip() != '':
-        return parseOperation(string[i:].strip())+int(string[:i])
-    return int(string[:i])
+    try:
+        if string[i:].strip() != '':
+            return parseOperation(string[i:].strip())+int(string[:i])
+        return int(string[:i])
+    except:
+        return "Hatalı girdi."
 
-def parseOperation( string):
+def parseOperation(string):
+    if not string:
+        return "Hatalı girdi: Boş işlem"
     if string[0] == "+":
         return parseNumber(string[1:].strip())
     elif string[0] == "-":
         return parseNumber(string[1:].strip())*-1
 
-print("Hesap Makinesi")
-expr = input("\nLütfen işlemi giriniz:")
-    
-number = parseNumber(expr)
-print(number)
+while True:
+    print("\nHesap Makinesi")
+    expr = input("Lütfen işlemi giriniz: ") 
+    print("\nCevap: ", parseNumber(expr))
